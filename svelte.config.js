6a289d47776a67547bb6cd753c164d03f6866927
afreen sultana <nayeemataj024@gmail.com>
2025-02-22 14:29:50 +0530

    i c

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,16 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+  preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter(),
-		alias: {
-			$lib: 'src/lib'
-		}
-	}
+  kit: {
+    adapter: adapter(), // Use Vercel adapter
+    alias: {
+      $lib: 'src/lib', // Allows importing from $lib instead of long paths
+    },
+  },
 };
 
 export default config;
